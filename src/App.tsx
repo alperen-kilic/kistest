c9fa08855bacc269205fedc04101ae93ecd821c7
Alperen Kılıç <alp.kilic.97@gmail.com>
2024-10-18 11:31:08 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ height: "100vh", minHeight: "100vh" }}>
+      <iframe
+        title="Kis"
+        width={"100%"}
+        height={"100%"}
+        src="https://cbs.beylikduzu.istanbul/GiSoftGis/#/cityguidepublic"
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
